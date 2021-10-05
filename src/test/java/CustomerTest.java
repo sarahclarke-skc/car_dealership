b2e1c0221703e16e1999b80f60a27ce166c3f889
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-05 12:04:21 +0100

    Customer tests passing

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import vehicle.Vehicle;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -21,6 +22,16 @@ public class CustomerTest {
     @Test
     public void hasName() {
         assertEquals("Tom Smith", customer.getName());
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(25000.00, customer.getBudget(), 0.001);
+    }
+
+    @Test
+    public void hasEmptyArrayList() {
+        assertEquals(0, customer.getVehicles().size());
     }
 
 }
