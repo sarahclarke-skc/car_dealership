e72e0de816c64a1a4dc8b5b8db6563763fbac8df
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-05 13:58:13 +0100

    Electric Tests passing

--- src/main/java/vehicle/vehicleType/Electric.java
+++ src/main/java/vehicle/vehicleType/Electric.java
@@ -1,4 +1,17 @@
 package vehicle.vehicleType;
 
-public class Electric {
+import vehicle.Vehicle;
+
+public class Electric extends Vehicle {
+
+    private EngineType engineType;
+
+    public Electric(String make, String model, int year, double price, TyreType tyreType) {
+        super(make, model, year, price, tyreType);
+        this.engineType = EngineType.ELECTRIC;
+    }
+
+    public String getFuelSource() {
+        return engineType.getFuelSource();
+    }
 }
--- src/test/java/vehicle/vehicleTypeTest/ElectricTest.java
+++ src/test/java/vehicle/vehicleTypeTest/ElectricTest.java
@@ -1,4 +1,49 @@
 package vehicle.vehicleTypeTest;
 
+import org.junit.Before;
+import org.junit.Test;
+import vehicle.vehicleType.Electric;
+import vehicle.vehicleType.TyreType;
+
+import static org.junit.Assert.assertEquals;
+
 public class ElectricTest {
+
+    Electric electric;
+
+    @Before
+    public void before() {
+        electric = new Electric("Tesla", "S", 2021, 75000.00, TyreType.SUMMER);
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Tesla", electric.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("S", electric.getModel());
+    }
+
+    @Test
+    public void hasYear() {
+        assertEquals(2021, electric.getYear());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(75000.00, electric.getPrice(), 0.001);
+    }
+
+    @Test
+    public void hasTyreType() {
+        assertEquals(TyreType.SUMMER, electric.getTyreType());
+    }
+
+    @Test
+    public void hasPetrolAndElectricEngine() {
+        assertEquals("Electricity", electric.getFuelSource());
+    }
 }
+
