2a01b6baff12e8b8b0451ada9ae28286ec9b2ab9
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-10-05 12:12:50 +0100

    Dealership tests passing

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,2 +1,37 @@
+import vehicle.Vehicle;
+
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private String name;
+    private Double till;
+    private ArrayList<Vehicle> vehicleArrayList;
+
+    public Dealership(String name, Double till, ArrayList<Vehicle> vehicleArrayList) {
+        this.name = name;
+        this.till = till;
+        this.vehicleArrayList = vehicleArrayList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getTill() {
+        return till;
+    }
+
+    public ArrayList<Vehicle> getVehicleArrayList() {
+        return vehicleArrayList;
+    }
+
+    public void setTill(Double till) {
+        this.till = till;
+    }
+
+    public void setVehicleArrayList(ArrayList<Vehicle> vehicleArrayList) {
+        this.vehicleArrayList = vehicleArrayList;
+    }
 }
+
--- src/test/java/DealershipTest.java
+++ src/test/java/DealershipTest.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+import vehicle.Vehicle;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DealershipTest {
+
+    Dealership dealership;
+    ArrayList<Vehicle> vehicleArrayList;
+
+    @Before
+    public void before() {
+        vehicleArrayList = new ArrayList<>();
+        dealership = new Dealership("Arnold CodeClan", 100000.00, vehicleArrayList);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Arnold CodeClan", dealership.getName());
+    }
+
+    @Test
+    public void hasTill() {
+        assertEquals(100000.00, dealership.getTill(), 0.001);
+    }
+
+    @Test
+    public void hasEmptyVehicleList() {
+        assertEquals(0, dealership.getVehicleArrayList().size());
+    }
 }
